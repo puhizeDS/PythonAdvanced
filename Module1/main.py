2a5e9bc77061a2981721cf889d25a7feb2c1134d
puhizeDS <puhiza.rexha@digitalschool.tech>
2024-10-18 17:00:34 +0200

    added variables lists

---
+++ Module1/main.py
@@ -0,0 +1,29 @@
+emri = "Puhiza"
+mosha = 50
+temperatura = 22.5
+
+print(emri)
+print(type(emri))
+print(type(mosha))
+print(type(temperatura))
+
+mosha = mosha + 10
+
+print(mosha)
+
+emri_mbiemri = emri + " Rexha"
+
+print(emri_mbiemri)
+
+my_data = ["Puhiza", "Rexha", 23, "Shkolla Digjitale"]
+print(my_data)
+my_data.append("Prishtine")
+print(my_data)
+my_data.insert(4,"Klasa 12")
+print(my_data)
+
+my_data.remove("Puhiza")
+
+del my_data[0]
+
+print(my_data)
